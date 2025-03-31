16aaa86eff235702b7f7cad1524f97e39eeb45df
Andrew Bartsch <andrewbartsch01@gmail.com>
2025-03-31 17:48:35 -0500

    added a test file for sound library testing added a file for 440 hertz sound and a folder for the sounds to be stored in

---
+++ soundTest.py
@@ -0,0 +1,13 @@
+#winsound testing
+# windsound doesn't work with what i need it to do
+# import winsound
+#
+#x = 0
+#while x < 5:
+#    winsound.Beep(440, 1000)
+#    x += 1
+
+
+#playsound testing
+from playsound import playsound
+playsound('a440.wav')
