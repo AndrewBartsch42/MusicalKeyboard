a3c1d5ade99344db2e74a8d65ac5eb8fbc0ad437
Andrew Bartsch <andrewbartsch01@gmail.com>
2025-03-31 16:20:59 -0500

    fixed typo in the main.py file name added a keyboard listener via the pynput library examples

---
+++ main.py
@@ -0,0 +1,20 @@
+from pynput import keyboard
+
+
+
+def on_press(key): #sends the key on press to print out in the console
+    try:
+        print('alphanumeric key {0} pressed'.format(key.char))
+    except AttributeError:
+        print('special key {0} pressed'.format(key))
+
+def on_release(key): # sends the key on release to the print out in the console
+    print('{0} released'.format(key))
+    if key == keyboard.Key.esc: # esc key is the exit condition for the loop
+        # stop listener 
+        return False
+    
+with keyboard.Listener( # starts the listener
+    on_press=on_press,
+    on_release=on_release) as listener:
+        listener.join()
