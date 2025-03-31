618cab382064d7bf2da036e376fcd93d71e1d105
Andrew Bartsch <andrewbartsch01@gmail.com>
2025-03-31 17:50:24 -0500

    changed main.py to test the windsound library added a txt file to track the hertz values of the sounds

--- main.py
+++ main.py
@@ -1,12 +1,20 @@
-from pynput import keyboard
+from pynput import keyboard # https://pynput.readthedocs.io/en/latest/index.html
 
+import winsound
 
 
 def on_press(key): #sends the key on press to print out in the console
-    try:
-        print('alphanumeric key {0} pressed'.format(key.char))
-    except AttributeError:
-        print('special key {0} pressed'.format(key))
+    if key == 'a':
+         winsound.Beep(440, 500)
+    else:
+        try:
+            #print('alphanumeric key {0} pressed'.format(key.char))
+            if "{0}".format(key.char) == "a":
+                winsound.Beep(440, 500)
+            else:
+                print('alphanumeric key {0} pressed'.format(key.char))
+        except AttributeError:
+            print('special key {0} pressed'.format(key))
 
 def on_release(key): # sends the key on release to the print out in the console
     print('{0} released'.format(key))
@@ -18,3 +26,4 @@ with keyboard.Listener( # starts the listener
     on_press=on_press,
     on_release=on_release) as listener:
         listener.join()
+
